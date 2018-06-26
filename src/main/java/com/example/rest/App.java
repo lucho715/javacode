cbeb2e2e1fb03c9907afe4e27ecf174f8d61ae07
lucho715 <40593995+lucho715@users.noreply.github.com>
2018-06-26 11:08:50 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -51,7 +51,7 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "http://140.86.15.104:3000/shield/33/45/blue/lucho715";
+        String url = "http://140.86.15.104:3000/shield/33/0/blue/lucho715";
         CloseableHttpResponse response = null;
  
         try {
